5eafef4d0c4478f268fbd2651b27fb895a82e6d9
Raphaëlle Lebailly <lebaillyraphaelle@gmail.com>
2024-05-02 15:24:32 -0400

    Update Test-SDM.R

--- Test-SDM.R
+++ Test-SDM.R
@@ -1,11 +1,13 @@
 # SDM FOR AQUACULTURE CANDIDATE SPECIES
-
+# setwd("C:/Users/User/Desktop/Internship/Data")
 # PACKAGES ----------------------------------------------------------------
 # Data Management
 library(tidyverse)
 library(conflicted) 
 library(letsR)
+library(foreach)
 library(DescTools)
+library(rfishbase)
 # Plot Maps
 library(plotly)
 library(maps)
@@ -40,7 +42,7 @@ GetEnvData <- function(variable, country, resolution){
 }
 # Species data
 GetSpData <- function(species){
-  data <- occ_data(scientificName = species)
+  data <- rgbif::occ_data(scientificName = species)
   return(data)
 }
 
@@ -120,9 +122,10 @@ Sprast <- function(sp, raw){
 
 ### Species data flagging
 Getflag <- function(data){
-  ##### Replace alpha-2 with alpha-3
+  # Replace alpha-2 with alpha-3
   indices <- match(data$countryCode, countcode$a2)
   data$countryCode <- countcode$a3[indices]
+  # Flags
   flags <- clean_coordinates(x = data, 
                                 lon = "decimalLongitude", 
                                 lat = "decimalLatitude",
@@ -133,9 +136,9 @@ Getflag <- function(data){
 }
 
 ### Add species name in final dataframe ----------------------------------
-Final.df <- function(final, sp){
+Final.df <- function(final, sp, base){
   coord <- matrix(c(sp$x, sp$y), ncol = 2) # Coordinates from species df
-  s <- cellFromXY(temp.min, xy = coord)
+  s <- cellFromXY(base, xy = coord)
   p <- which(!is.na(s))
   s1 <- s[p]
   final$species <- NA
@@ -151,12 +154,15 @@ Final.df <- function(final, sp){
 
 ### Import raster data ---------------------------------------------------------------
 ## Environmental data
-tmin <- GetEnvData("tmin", "Vietnam", 0.5)
-wind <- GetEnvData("wind", "Vietnam", 0.5)
+# tmin <- GetEnvData("tmin", "Vietnam", 0.5)
+tmin <- worldclim_country("Vietnam", var = "tmin", res = 0.5, path=tempdir())
+# wind <- GetEnvData("wind", "Vietnam", 0.5)
+wind <- worldclim_country("Vietnam", var = "wind", res = 0.5, path=tempdir())
 ## Species data
 mola <- GetSpData("Amblypharyngodon mola")
 Hg <- GetSpData("Hemibagrus guttatus")
-
+# mola <- rgbif::occ_data(scientificName = "Amblypharyngodon mola" )
+# Hg <- rgbif::occ_data(scientificName = "Hemibagrus guttatus")
 
 ### Set base  ----------------------------------------------------------
 ## Layer
@@ -170,7 +176,7 @@ wind.rs <- resample(wind, BASE, "bilinear") # Adapt one raster's geometry to the
 
 ## Subset mean value (optional)
 # Minimal temperature
-tmin.df <- as.data.frame(temp.min, xy = TRUE) # The first dataframe
+tmin.df <- as.data.frame(tmin, xy = TRUE) # The first dataframe
 tmin.mn <- mean.df(tmin, "tmin", "df") # # Compute the mean value and keep as a dataframe (useful?)
 tmin.mnr <- mean.df(layer = tmin, arg = "tmin", type = "raster") # Compute the mean value and re-transform into a SpatRaster object
 Mapplot(tmin.mn, "VNM") # Plot the layer (works with df or raster)
@@ -184,7 +190,7 @@ Mapplot(wind.mn, "VNM")
 # Add both layers into the same dataframe 
 env <- tmin.mn %>%
   left_join(wind.mn)
-env2 <- cbind(tmin.mean, mean_wind = wind.mn$mean_wind) # Method 2
+env2 <- cbind(tmin.mn, mean_wind = wind.mn$mean_wind) # Method 2
 
 # Convert back to a raster with all the environmental variables
 env.rast <- as_spatraster(env, crs = "EPSG:4326")
@@ -213,7 +219,7 @@ Hg.flags <- Getflag(Hg.cc) # 0 flag
 sp <- rbind(mola.df2, Hg.df2)
 
 # Assemble final data frame with environmental values + species
-finaldf <- Final.df(env, sp) 
+finaldf <- Final.df(env, sp, BASE) 
 # Convert to raster
 finalr <- as_spatraster(finaldf, crs = "EPSG:4326")
 finalr
@@ -228,3 +234,55 @@ Mapplot(finalr, "VNM")
 
 ### Visualization of the data with rgbif ---------------------------------
 
+
+# Background data ---------------------------------------------------------
+# Use the rfishbase package to import all the fish data (30 000 sp of fishes)
+# species of fish, their biology, ecology, morphology, and more. This package also
+# supports experimental access to 'SeaLifeBase' data, which contains
+# nearly 200,000 species records for all types of aquatic life not covered by
+# 'FishBase.'
+
+# Background data 
+bg <- fb_tbl("species")
+bg$namesp <- paste(bg$Genus, bg$Species, sep = " ")
+
+
+# saveRDS(bg, file = "fishbase.rds")
+
+summary(bg)
+dim(bg)
+unique(bg$UsedforAquaculture) # Indication on what's used and what has potential
+fish_sp <- bg$namesp # Vector with species names for all fishes
+
+# Download the species required
+# distrifish <- rgbif::occ_data(scientificName = fish_sp)
+# saveRDS(distrifish, file = "fishbase.rds") # Save GBIF data for 
+distrifish <- readRDS("fishbase.rds") 
+
+# Fit all of this in a grid
+
+
+
+# SDM test ----------------------------------------------------------------
+
+# bg_aqua <- bg %>%
+#   tidyterra::filter(UsedforAquaculture == "commercial")
+# dim(bg_aqua) # 358 entries
+# # Add column with both genus and species name
+# bg_aqua$namesp <- paste(bg_aqua$Genus, bg_aqua$Species, sep = " ")
+# unique(bg_aqua$namesp)
+# # Retrieve this as a vector
+# aq_sp <- bg_aqua$namesp
+# # Import distribution data for this list of species (GBIF or FishBase?)
+# dist_aqua <- rgbif::occ_data(scientificName = aq_sp)
+# saveRDS(dist_aqua, file = "aquafish.rds")
+setwd("C:/Users/User/Documents")
+dist_aqua <- readRDS("aquafish.rds")
+
+test <- as.data.frame(dist_aqua, xy = TRUE)
+# Introduce each species in tje aquaculture dataframe into the SDM
+
+## Fit into base layer geometry
+sp <- Sprast(sp = dist_aqua, )
+## Clean the species data
+Getflag(dist_aqua, )
