86c3c99c31b17591a112bfe5c73f57947cf318dc
Raphaëlle Lebailly <lebaillyraphaelle@gmail.com>
2024-05-25 02:01:35 -0400

    Update Run_SDM.R

--- Run_SDM.R
+++ Run_SDM.R
@@ -45,29 +45,27 @@ PO4 <- rast(paste0(dir,"/po4_baseline_2000_2018_depthsurf_6006_d51b_00e9_U171644
 SI <- rast(paste0(dir,"/si_baseline_2000_2018_depthsurf_395f_f84b_becc_U1716440390984.nc"))
 bathy <- rast(paste0(dir,"/terrain_characteristics_bea1_f9a7_03c1_U1716440607679.nc"))
 surftemp <- rast(paste0(dir,"/thetao_baseline_2000_2019_depthsurf_74ff_39fa_9adc_U1716440102349.nc"))
+prim_prod <- rast(paste0(dir,"/phyc_baseline_2000_2020_depthsurf_7d39_02af_cdbd_U1716500021103.nc"))
 # List of variables
-aqua_env <- list(NO3, PO4, SI, bathy, surftemp)
+aqua_env <- list(NO3, PO4, SI, bathy, surftemp, prim_prod)
 
-#### Select subsets of the rasters (so that we can work with it) ==> AUTOMATIZE (list rasters to crop)
-vietnam_extent <- ext(102, 110, 8, 24)
-# WorldClim data
-tminVNM <- crop(tmin, vietnam_extent)
-tmaxVNM <- crop(tmax, vietnam_extent)
-rm(tmin)
-rm(tmax)
+NO3_df <- as.data.frame(NO3, xy = TRUE)
+
+# Retreive data for countries extent
+world <- ne_countries(scale = "medium", returnclass = "sf")
+world_vect <- vect(world)
+regions <- world$name
+
+# WorldClim data (continental data)
+cont_env <- list(tmin, tmax)
+env_cropped2 <- GetCroppedRaster(cont_env, 'India') # A bit longer because the size of the objects is bigger
+rm(tmin) ; rm(tmax)
 gc()
-# Bio-ORACLE data
-NO3VNM <- crop(NO3, vietnam_extent)
+plot(env_cropped2[[2]])
 
-GetCroppedRaster <- function(list_raster, extent){
-  rast_ext <- list()
-  for(i in 1:length(list_raster)){
-    rast_ext <- c(rast_ext, crop(list_raster[i], extent))
-  }
-  return(rast_ext)
-}
+env_cropped <- GetCroppedRaster(aqua_env, 'India') # Start with caps on for the country
+plot(env_cropped[[5]])
 
-test <- GetCroppedRaster(aqua_env, vietnam_extent)
 
 # FUNCTIONS ---------------------------------------------------------------
 ### LOADED FROM SOURCE CODE FOR THE FUNCTIONS
@@ -77,18 +75,33 @@ test <- GetCroppedRaster(aqua_env, vietnam_extent)
 
 #### Environmental data management
 # 1. Set base
-BASE <- tminVNM
+BASE <- env_cropped2[[1]]
 
 # 2. Adjust geometry of the other rasters
-tmax.rs <- resample(tmaxVNM, BASE, "bilinear")
+# tmax.rs <- resample(env_cropped2[[2]], BASE, "bilinear")
+# Resample all of the environmental data given the base layer
+env_rs <- list()
+for (i in seq_along(env_cropped)) {
+  env_rs[[i]] <- resample(env_cropped[[i]], BASE, "bilinear") # OK
+}
 
 # 3. Get mean value
-tmin.mn <- GetMeanDf(tminVNM, "tmin", "df")
-tmax.mn <- GetMeanDf(tmax.rs, "tmax", "df") 
+# tmin.mn <- GetMeanDf(tminVNM, "tmin", "df")
+# tmax.mn <- GetMeanDf(tmax.rs, "tmax", "df") 
+# For the list of env data
+# NO NEED FOR BIO-ORACLE DATA, ALREADY GOOD FORMAT (get the dataframes only)
+env_mn <- list()
+for (i in seq_along(env_rs)) {
+  env_mn[[i]] <- as.data.frame(env_rs[[i]], xy = TRUE)
+}
+
 
 # 4. Merge dataframes (mean or not)
-env <- tmin.mn %>%
-  left_join(tmax.mn)
+# env <- tmin.mn %>%
+#   left_join(tmax.mn)
+
+# Try to find optimized function
+env_mg <- GetMerged(env_mn, group_size = length(aqua_env))
 
 # 5. Prepare the species data
 # Aquaspecies are the species of interest
@@ -96,66 +109,68 @@ env <- tmin.mn %>%
 # These data have already been cleaned (delete the flags etc.)
 
 # 6. Assemble df with species + environmental data = Presence dataframe
-pres <- GetCombinedDf(env, aquaspecies_df, BASE)
-# pres <- pres[,-3]
+pres <- GetCombinedDf(env_mg, aquaspecies_df, BASE) # A bit long, could use some optimizing
+
 
 # 7. Get Pseudo-absences df
 # Select random subset of the background dataset (n = 10,000)
 set.seed(10)
-
-vietnam_extent <- list(
-  min_lon = 102,
-  max_lon = 110,
-  min_lat = 8,
-  max_lat = 24
-)
 # Get the background subset
-subbg <- GetSubBg(bg_df, vietnam_extent)
+subbg <- GetSubBg(bg_df, 'India')
 # Get the pseudoabsences dataframe
-pseudoabs <- GetCombinedDf(env, subbg, BASE)
+pseudoabs <- GetCombinedDf(env_mg, subbg, BASE)
 
 # Check for NAs
+colSums(is.na(pres))
+colSums(is.na(pseudoabs))
 colSums(!is.na(pres))
 colSums(!is.na(pseudoabs))
 
 # 8. Get Presence/Absence dataframe
-dat <- GetModelData(pseudoabs, pres)
+# Get the list of the combined dataframes
 
-
+dat <- GetModelData(pseudoabs, pres) # Works but 20 occurrences might be too much, or try to counter balance with more aquaculture species
+# Here, 5 occurrences to have at least some data in the list 
 
 
 # Clean the environment
 # rm(list = ls())
 # gc()
 
-# Get the data for the targeted country (on hold for now)
-# VNM <- gadm(country = "VNM", level = 0, path=tempdir())
-# bg_rast <- as_spatraster(bg_df, crs = "EPSG:4326")
-# var.border <- mask(bg_rast, VNM)
-# test <- st_as_sf(bg_df, coords = c("x", "y"), crs = "EPSG:4326")
-# subset_sf <- st_intersection(test, VNM)
-# 
-# # Convertir le résultat de retour en dataframe si nécessaire
-# subset_df <- as.data.frame(subset_sf)
-
 # MODEL -------------------------------------------------------------------
 
+# Faire un lapply pour le modele avec la liste de dataframes dat[[i]] acces dans la liste des dfs
 sim_func <- function(names_x, name_y,dat){
-  tmp_sdm<-gam(formula(paste(name_y,"~",paste(names_x,collapse='+'))),family = binomial, data=dat,select = TRUE, method="GCV.Cp")
+  tmp_sdm <- gam(formula(paste(name_y,"~",paste(names_x,collapse='+'))),family = binomial, data=dat,select = TRUE, method="GCV.Cp")
   return(tmp_sdm)
 }
-name_y = "PA"
-names_x= c("mean_tmin","mean_tmax") # Then you can set your list of predictor variables
-names_x1=paste("s(",names_x,",k=5)",sep="") # and for all variables that you want to allow a non-linear fit
-sdm_obj=sim_func(names_x1,name_y, dat) # get back the sdm object
+name_y <- "PA"
+col_names <- colnames(dat[[1]]) # Retrieve colnames to have variable names
+selected_col_names <- col_names[3:(length(col_names) - 2)]
+names_x <- selected_col_names # Then you can set your list of predictor variables
+names_x1 <- paste("s(",names_x,",k=5)",sep="") # and for all variables that you want to allow a non-linear fit
+# Do a loop to get all the predictions for the whole list of dataframes
+sdm_obj <- list()
+for (i in seq_along(dat)) {
+  sdm_obj[[i]] <- sim_func(names_x1,name_y, dat[[i]])
+}
+sdm_obj2=sim_func(names_x1,name_y, dat[[1]]) # get back the sdm object
 # Newdat is supposed to be the full coverage of the area of interest (rasters covering the whole area)
-Newdat <- env
-pred <- predict.gam(sdm_obj, newdata=Newdat) # new data would be the environments that you want to extrapolate the model to.
+Newdat <- env_mg
+# Loop for predictions
+pred <- list()
+Newpred <- rep(list(Newdat), length(dat))
+for (i in seq_along(sdm_obj)) {
+  pred[[i]] <- predict.gam(sdm_obj[[i]], newdata=Newdat) # Maybe not the quickest way
+  Newpred[[i]]$predictions <- pred[[i]]
+}
+
+pred2 <- predict.gam(sdm_obj[[1]], newdata=Newdat) # new data would be the environments that you want to extrapolate the model to.
 Newdat$predictions <- pred # Add the predictions to the new data
 
 # Plot the results
 x11()
-ggplot(Newdat, aes(x = x, y = y, fill = predictions)) +
+ggplot(Newpred[[2]], aes(x = x, y = y, fill = predictions)) + # So freaking long to display
   geom_tile() +
   scale_fill_viridis_c() +
   labs(title = "Probability of predicted presence of the species",
