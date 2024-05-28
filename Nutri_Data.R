191ee158310c4b2c7ee15a3bc17c1fb1204700a0
Raphaëlle Lebailly <lebaillyraphaelle@gmail.com>
2024-05-28 13:50:00 -0400

    Update Nutri_Data.R

--- Nutri_Data.R
+++ Nutri_Data.R
@@ -41,6 +41,10 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 # Get the nutritional data
 setwd("C:/Users/User/Desktop/Internship/Data/Nutrition")
 
+
+### GLOBAL NUTRITIONAL NEEDS --------------------------------------------
+
+##### MICRONUTRIENTS ----------------------------------------------------------
 # Load data for global deficiencies
 # beta_carotene <- read.csv("Beta-carotene_Combined.csv"? stringsAsFactors = TRUE) # Not used because obsolete
 ferritin <- read.csv("Ferritin_Combined.csv", stringsAsFactors = TRUE)
@@ -48,9 +52,35 @@ haemoglobin <- read.csv("Haemoglobin_Combined.csv", stringsAsFactors = TRUE)
 retinol <- read.csv("Retinol_Combined.csv", stringsAsFactors = TRUE)
 zinc <- read.csv("Zinc_Combined.csv", stringsAsFactors = TRUE)
 
+# Ferritin
+ferritin_clean <- ferritin %>%
+  tidyterra::filter(Survey.Year >= 2000) %>% # Chose nb of occurrences depending on the 
+  ungroup()
+macro <- droplevels(ferritin_clean)
+rm(ferritin)
 
-# MACRONUTRImacroS ----------------------------------------------------------
+# Haemoglobin
+haemoglobin_clean <- haemoglobin %>%
+  tidyterra::filter(Survey.Year >= 2000) %>% # Chose nb of occurrences depending on the 
+  ungroup()
+macro <- droplevels(haemoglobin_clean)
+rm(haemoglobin)
 
+# Retinol
+retinol_clean <- retinol %>%
+  tidyterra::filter(Survey.Year >= 2000) %>% # Chose nb of occurrences depending on the 
+  ungroup()
+macro <- droplevels(retinol_clean)
+rm(retinol)
+
+# Zinc
+zinc_clean <- zinc %>%
+  tidyterra::filter(Survey.Year >= 2000) %>% # Chose nb of occurrences depending on the 
+  ungroup()
+macro <- droplevels(zinc_clean)
+rm(zinc)
+
+##### MACRONUTRIENTS ----------------------------------------------------------
 macro_df <- read.csv("macro_data.csv", stringsAsFactors = TRUE)
 # Filter data and delete what's before 2000
 macro_df_clean <- macro_df %>%
@@ -110,10 +140,10 @@ ggsave(file="test.pdf", plot=image, width=10, height=8)
 
 
 
-# Read data for fish composition
-setwd("C:/Users/User/Desktop/Internship/Data/Nutrition")
-df_species <- read.table("sp_names.csv", sep = ";", header = TRUE)
-fish_compo <- read.table("fish_composition.csv", sep = ";", dec = ",", header = TRUE)
+
+
+
+### FISH COMPOSITION ----------------------------------------------------
 ### Selection
 ## Micronutrimacros
 # ZN(mg) = Zinc
@@ -129,16 +159,32 @@ fish_compo <- read.table("fish_composition.csv", sep = ";", dec = ",", header = 
 # FAT(g) = total fat
 # CHOAVLDF(g) = available carbohydrates
 
+# Read data for fish composition
+# setwd("C:/Users/User/Desktop/Internship/Data/Nutrition")
+df_species <- read.table("sp_names.csv", sep = ";", header = TRUE, na.strings=c("","NA")) # References species
+fish_compo <- read.table("fish_composition.csv", sep = ";", dec = ",", header = TRUE) # Fish composition
+# Cut after "FAUN(g)" (fatty acids non identified) column 66
+fish_compo_clean <- fish_compo[,0:66]
+rm(fish_compo)
 
-# Cut after "FAUN(g)" (fatty acids non idmacroified) column 66
-fish_compo_clean <- fish_compo[,-66:188]
 
+# Retrieve the list of latin names for english names
+df_species<- df_species[,0:10]
+
+lat_names <- df_species$SCIENTIFIC.NAME..AUTHOR
+eng_names <- df_species$ENGLISH.NAME
+# Get the dataframe of the fish species names
+names <- data.frame(lat_names, eng_names)
+names <- names[- which(is.na(names$lat_names)),] # Remove NA rows
+names$lat_names <- str_extract(names$lat_names, "^[A-Za-z]+\\s[A-Za-z]+")
+names <- names %>%
+   tidyterra::filter(!grepl("Includes", lat_names)) # Chose nb of occurrences depending on the
+names <- droplevels(names)
 
 
 # Define levels "low", "medium" and high
 # Use mutate() function to create a new column with conditions
 
-
-# Filter the dataset with only these componmacros
+# Filter the dataset with only these components
 
 
