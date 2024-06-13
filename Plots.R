2f37a15882206bd20d6ebdcd1a6714f7d06df449
Raphaëlle Lebailly <lebaillyraphaelle@gmail.com>
2024-06-13 08:35:10 -0400

    Update Plots.R

--- Plots.R
+++ Plots.R
@@ -25,12 +25,37 @@ tmin <- rast(raster_tmin)
 
 
 
-# Add the ISOCODE3 to map the aquaspecies data
 # Créer un objet spatial à partir des coordonnées x et y
 coordinates(aquaspecies_df) <- ~ x + y
 proj4string(aquaspecies_df) <- CRS("+proj=longlat +datum=WGS84")
 # Charger les frontières des pays à partir de Natural Earth
 world <- ne_countries(scale = "medium", returnclass = "sf")
+countries <- vect("C:/Users/User/Desktop/Internship/Data/Regions/world-administrative-boundaries/world-administrative-boundaries.shp")
+
+# Density plot (without ne_countries function)
+countries <- geodata::world(resolution = 5, level = 0, path = tempdir()) # Try to map with this
+# Convert dataframe to spatial vector
+
+points <- vect(data, geom = c("x", "y"), crs = crs(countries))
+ext <- ext(countries)
+res <- 0.5  # résolution du raster (vous pouvez ajuster selon vos besoins)
+
+# Créer un raster vide avec l'extension et la résolution désirées
+country_raster <- rast(ext, res = res)
+t <- as.data.frame(countries, xy=TRUE)
+# Rasteriser les pays, chaque cellule du raster reçoit la valeur de l'ID du pays
+country_raster <- rasterize(countries, country_raster, field = "iso3")
+points_raster <- rasterize(points, country_raster, fun = "length", background = 0)
+
+density_by_country <- zonal(points_raster, country_raster, fun = sum)
+colnames(density_by_country)[2] <- "density"
+
+
+
+
+
+
+
 # Convertir df en sf object pour l'intersection
 df_sf <- st_as_sf(aquaspecies_df)
 # Faire l'intersection pour obtenir les noms des pays
@@ -81,3 +106,74 @@ image <- ggplot() +
     legend.text = element_text(angle = 315, vjust = 0)
   ) 
 print(image)
+
+
+
+
+# Tests -----------------------------------
+
+aquaspecies_df <- read_rds("aquaspecies_df.rds")
+
+# Obtenir les données des frontières des pays
+mapdata <- map_data("world") # 252 countries
+world <- ne_countries(scale = "medium", returnclass = "sf")
+# Convertir les coordonnées en objets sf
+points_sf <- st_as_sf(aquaspecies_df, coords = c("x", "y"), crs = 4326)
+# Déterminer le pays pour chaque point
+joined <- st_join(points_sf, world, join = st_within)
+# Extraire le nom du pays et ajouter à la table initiale
+aquaspecies_df$region <- joined$name_long
+head(aquaspecies_df)
+length(which(is.na(aquaspecies_df$region))) # 5 uniquement
+# aquaspecies_df <- aquaspecies_df %>% tidyterra::filter(!is.na(map_data$count))
+map_data <- left_join(mapdata, aquaspecies_df, by= "region") # Gros dataset
+gc()
+head(map_data)
+map_data <- map_data[,-7] # Delete count (why here ??)
+# Compter le nombre de points par pays
+counts_by_country <- map_data %>%
+  group_by(region) %>%
+  summarise(count = n())
+head(counts_by_country)
+
+# Recommencer la méthode avec counts_per_country
+rm(points_sf,joined)
+rm(map_data)
+gc()
+map_data <- left_join(mapdata, counts_by_country, by= "region")
+length(which(is.na(counts_by_country$count))) # 0
+# mapdata1 <- map_data %>% tidyterra::filter(!is.na(map_data$count)) (do if presence of NAs)
+which(map_data$region == "Antartica") # No Antartica
+
+
+# Start plotting
+map1 <- ggplot(map_data, aes(x = long, y = lat, group = group)) +
+  borders("world", colour = "gray85", fill = "gray80") +
+  theme_map() +
+  coord_cartesian(ylim = c(-55, 85)) + # Delete Antartica (some rendering problems)
+  geom_polygon(aes(fill = count), color = "black") +
+  # Normal scale
+  # scale_fill_gradient(name = "Number of samples", low = "yellow", high = "red", na.value = "grey50")+
+  # Log scale
+  scale_fill_gradientn(
+    name = "Number of samples \n (log scale)",
+    colors = c("yellow", "goldenrod1" ,"red"),
+    trans = "log10",
+    na.value = "grey50",
+    breaks = scales::trans_breaks("log10", function(x) 10^x),
+    labels = scales::trans_format("log10", scales::math_format(10^.x))
+  ) +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        rect = element_blank(),
+        panel.background = element_rect(fill = "lightcyan"),
+        panel.grid = element_line(color = "white"))
+map1
+
+ggsave(file="species_density_per_country.pdf", plot=map1, width=13, height=6)
+
+# scale_fill_gradient(name = "Number of samples", low = "yellow", high = "red", na.value = "grey50")+
+  
